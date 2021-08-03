26a69cc0c22ad9ec4c916f6e926fc874fd0c12b7
Rullay <88244082+Rullay@users.noreply.github.com>
2021-08-03 18:14:02 +0300

    ChangeButton

---
+++ Assets/_Scripts/ChangeButton.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeButton : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> Hands_1;
+    [SerializeField] private List<GameObject> Hands_2;
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void ChangeHands()
+    {
+        if(Hands_1[0].activeSelf == true)
+        {
+            for(int i = 0; i < Hands_2.Count; i++)
+            {
+                Hands_2[i].SetActive(true);
+                Hands_1[i].SetActive(false);
+            }
+        }
+        else 
+        {
+            for (int i = 0; i < Hands_2.Count; i++)
+            {
+                Hands_1[i].SetActive(true);
+                Hands_2[i].SetActive(false);
+            }
+        }
+    }
+}
