09a0cb55053b537fe8247caa039f3ca28f253451
Rullay <88244082+Rullay@users.noreply.github.com>
2021-08-03 21:46:57 +0300

    zalupa_Blyaty

--- Assets/_Scripts/Item.cs
+++ Assets/_Scripts/Item.cs
@@ -4,22 +4,69 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+
 public class Item : MonoBehaviour
 {
 
     [SerializeField] public Sprite item_Sprite;
-   
 
+    public string item_Type;
+
+    public enum ItemTypeEnum : int
+    {
+        MainHand,
+        OfHand,
+        Head ,
+        Chest,
+        Artifact 
+    }
+    public ItemTypeEnum itemType;
 
 
     void Start()
     {
+        Item_Type();
         
     }
 
-   
     void Update()
     {
-        
+
     }
-}
+
+    void Item_Type()
+    {
+        switch (itemType)
+        {
+            case ItemTypeEnum.MainHand:
+                item_Type = "MainHand";
+                break;
+
+            case ItemTypeEnum.OfHand:
+                item_Type = "OfHand";
+                break;
+
+            case ItemTypeEnum.Head:
+                item_Type = "Head";
+                break;
+
+            case ItemTypeEnum.Chest:
+                item_Type = "Chest";
+                break;
+
+            case ItemTypeEnum.Artifact:
+                item_Type = "Artifact";
+                break;
+        }
+    }
+
+    public void Ñollected()
+    {
+        gameObject.SetActive(false);
+    }
+
+
+ }   
+
+    
+
